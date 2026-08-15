68be7139c5ffaa48c2c3089e7178801715ca6d89
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:09:15 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,8 +13,6 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/SUPERZSNES
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
